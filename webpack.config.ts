dd33a4141070982733a11336d46d28f551a109ab
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2018-02-24 13:19:39 -0300

    update

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,11 @@
 import * as  path from 'path';
 import * as webpack from 'webpack';
+import * as UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+import * as  CompressionPlugin from 'compression-webpack-plugin';
+require('dotenv').config();
 
 const config: webpack.Configuration = {
     entry: './app/code/imports/imports.ts',
-    // entry: './app/code/onLoad/onLoad.ts',
     output: {
         libraryTarget: 'commonjs',
         path: path.resolve(__dirname, 'dist'),
@@ -38,7 +40,35 @@ const config: webpack.Configuration = {
                 ]
             }
         ]
-    }
+    },
+    plugins: []
 };
+
+if (JSON.parse(process.env.WEBPACK_PRODUCTION)) {
+    console.log('Production');
+    config.plugins.push(
+        new UglifyJSPlugin({
+            parallel: true,
+            uglifyOptions: {
+                ecma: 8,
+                mangle: false,
+                compress: false
+            }
+        }) // ,
+        // new CompressionPlugin({
+        //     asset: '[path].gz[query]',
+        //     algorithm: 'gzip',
+        //     test: /\.js$|\.css$|\.html$/,
+        //     threshold: 10240,
+        //     minRatio: 0
+        // })
+    );
+} else {
+    console.log('Development');
+    config.devtool = 'inline-source-map'
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    );
+}
 
 export default config;
