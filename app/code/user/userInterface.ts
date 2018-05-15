fc1fccb18d8adb9aac20e5923a2564650b782b32
Judahh <judahholanda7@gmail.com>
2018-05-15 17:02:21 -0300

    update

--- app/code/user/userInterface.ts
+++ app/code/user/userInterface.ts
@@ -54,7 +54,7 @@ export class UserInterface extends AppObject {
         let textComponent = component.getFather().getFather().arrayAppObject[0].arrayAppObject[0];
         let textElement = textComponent.getElement();
         // console.log(textElement.value);
-        let symptom = textElement.value;
+        let symptom = textElement.value;//alias
         //next
     }
 
@@ -73,6 +73,53 @@ export class UserInterface extends AppObject {
             referred: referred,
             depth: depth
         }
+        //next
+    }
+
+    public getQuantity(component: Component) {
+        console.log(component);
+        let fatherComponent = component.getFather().getFather().getFather();
+        let intesityElement = fatherComponent.arrayAppObject[0].arrayAppObject[0].arrayAppObject[0].getElement();
+        let frequencyElement = fatherComponent.arrayAppObject[1].arrayAppObject[0].arrayAppObject[0].getElement();
+        let numberElement = fatherComponent.arrayAppObject[2].arrayAppObject[0].arrayAppObject[0].getElement();
+        let intervalElement = fatherComponent.arrayAppObject[3].arrayAppObject[0].arrayAppObject[0].getElement();
+        let intesity = intesityElement.value;
+        let frequency = frequencyElement.value;
+        let number = numberElement.value;
+        let interval = intervalElement.value;
+
+        let quantity = {
+            intesity: intesity,
+            frequency: frequency,
+            number: number,
+            interval: interval
+        }
+        console.log(quantity);
+        //next
+    }
+
+    public getTime(component: Component) {
+        console.log(component);
+        let fatherComponent = component.getFather().getFather().getFather();
+        let startElement = fatherComponent.arrayAppObject[0].arrayAppObject[0].arrayAppObject[0].getElement();
+        let endElement = fatherComponent.arrayAppObject[1].arrayAppObject[0].arrayAppObject[0].getElement();
+        let start:Date = new Date(startElement.value);
+        let end:Date = new Date(endElement.value);
+
+        let time = {
+            start: start,
+            end: end
+        }
+        console.log(time);
+        //next
+    }
+
+    public getType(component: Component) {
+        console.log(component);
+        let fatherComponent = component.getFather().getFather().getFather();
+        let typeElement = fatherComponent.arrayAppObject[0].arrayAppObject[0].arrayAppObject[0].getElement();
+        let type = typeElement.value;//alias
+        console.log(type);
         //next
     }
 }
